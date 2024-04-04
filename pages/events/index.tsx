5fc4c2797703819c6772f8b862797253eb56549a
bdoddi <bdoddi@my.bridgeport.edu>
2024-04-04 01:21:34 -0400

    Checking auto deploy

--- pages/events/index.tsx
+++ pages/events/index.tsx
@@ -4,7 +4,7 @@ const Events = () => {
   return (
     <>
       <h1> Hearty Welcome to Event Planning Application.  </h1>
-      <h3> Discover brand new collection of events </h3>
+      <h3> Discover new collection of events </h3>
     </>
   )
 }
