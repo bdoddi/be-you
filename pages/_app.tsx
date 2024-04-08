9d4e43892250cb7f789e61e9752917c77a957079
bdoddi <bdoddi@my.bridgeport.edu>
2024-04-08 00:46:10 -0400

    updating git without aws

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -5,14 +5,58 @@ import { theme } from '../styles/theme'
 import { GlobalStyle } from '../styles/globalStyle'
 import Layout from '../shared/layout/Layout'
 import ErrorBoundary from '../shared/errorBoundary/ErrorBoundary'
-import { Amplify } from 'aws-amplify';
-import config from './aws-exports';
-import "@aws-amplify/ui-react/styles.css";
-import { withAuthenticator } from "@aws-amplify/ui-react";
 
-Amplify.configure(config);
+// import "@aws-amplify/ui-react/styles.css";
+// import { withAuthenticator } from "@aws-amplify/ui-react";
+
+// import { listNotes } from "../src/graphql/queries.js";
+// import {
+//   createNote as createNoteMutation,
+//   deleteNote as deleteNoteMutation,
+// } from "../src/graphql/mutations.js";
+// import { useEffect, useState } from 'react'
+
+// import { Amplify, API, graphqlOperation } from 'aws-amplify';
+// import awsconfig from './aws-exports';
+// Amplify.configure(awsconfig);
 
 function MyApp({ Component, pageProps }: AppProps) {
+  // const [notes, setNotes] = useState([]);
+
+  // useEffect(() => {
+  //   fetchNotes();
+  // }, []);
+
+  // async function fetchNotes() {
+  //   const apiData = await API.graphql({ query: listNotes });
+  //   //@ts-ignore
+  //   const notesFromAPI = apiData?.data.listNotes.items;
+  //   setNotes(notesFromAPI);
+  // }
+  // async function createNote(event: any) {
+  //   event.preventDefault();
+  //   const form = new FormData(event.target);
+  //   const data = {
+  //     name: form.get("name"),
+  //     description: form.get("description"),
+  //   };
+  //   await API.graphql({
+  //     query: createNoteMutation,
+  //     variables: { input: data },
+  //   });
+  //   fetchNotes();
+  //   event.target.reset();
+  // }
+
+  // async function deleteNote({ id }: { id: number}) {
+  //   //@ts-ignore
+  //   const newNotes = notes.filter((note) => note?.id !== id);
+  //   setNotes(newNotes);
+  //   await API.graphql({
+  //     query: deleteNoteMutation,
+  //     variables: { input: { id } },
+  //   });
+  // }
   return (
     <ThemeProvider theme={theme}>
       <GlobalStyle />
@@ -25,4 +69,5 @@ function MyApp({ Component, pageProps }: AppProps) {
   )
 }
 
-export default withAuthenticator(MyApp)
+// export default withAuthenticator(MyApp)
+export default MyApp
