38bb4496a53dac9269f5035b89ad1fa50c00ea99
bdoddi <bdoddi@my.bridgeport.edu>
2024-04-09 01:37:04 -0400

    Added graphql and storage

--- amplify/backend/types/amplify-dependent-resources-ref.d.ts
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -16,5 +16,11 @@ export type AmplifyDependentResourcesAttributes = {
       "UserPoolId": "string",
       "UserPoolName": "string"
     }
+  },
+  "storage": {
+    "beyouStorgae": {
+      "BucketName": "string",
+      "Region": "string"
+    }
   }
 }
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,15 +1,14 @@
-import '../styles/globalStyle.ts'
-import type { AppProps } from 'next/app'
-import { ThemeProvider } from 'styled-components'
-import { theme } from '../styles/theme'
-import { GlobalStyle } from '../styles/globalStyle'
-import Layout from '../shared/layout/Layout'
-import ErrorBoundary from '../shared/errorBoundary/ErrorBoundary'
+import "../styles/globalStyle.ts";
+import type { AppProps } from "next/app";
+import { ThemeProvider } from "styled-components";
+import { theme } from "../styles/theme";
+import { GlobalStyle } from "../styles/globalStyle";
+import Layout from "../shared/layout/Layout";
+import ErrorBoundary from "../shared/errorBoundary/ErrorBoundary";
 
 import React, { useState, useEffect } from "react";
 import "@aws-amplify/ui-react/styles.css";
-import { Amplify } from 'aws-amplify';
-import amplifyconfig from '../awsComponents/amplifyconfiguration.json';
+import amplifyconfig from "../awsComponents/amplifyconfiguration.json";
 import {
   Button,
   Flex,
@@ -23,10 +22,11 @@ import { listNotes } from "../src/graphql/queries.js";
 import {
   createNote as createNoteMutation,
   deleteNote as deleteNoteMutation,
-} from "../src/graphql/mutations.js"
-import { generateClient } from 'aws-amplify/api';
+} from "../src/graphql/mutations.js";
+import { Amplify, API, Storage } from "aws-amplify";
 
-const API = generateClient();
+import awsconfig from "../awsComponents/aws-exports.js";
+Amplify.configure(awsconfig);
 
 Amplify.configure(amplifyconfig);
 
@@ -41,16 +41,31 @@ function MyApp({ Component, pageProps }: AppProps) {
     const apiData = await API.graphql({ query: listNotes });
     //@ts-ignore
     const notesFromAPI = apiData?.data.listNotes.items;
+    await Promise.all(
+      notesFromAPI.map(async (note: any) => {
+        if (note.image) {
+          //@ts-ignore
+          const url = await Storage?.get(note.name);
+          note.image = url;
+        }
+        return note;
+      })
+    );
     setNotes(notesFromAPI);
   }
 
   async function createNote(event: any) {
     event.preventDefault();
     const form = new FormData(event.target);
+    const image = form.get("image");
     const data = {
       name: form.get("name"),
       description: form.get("description"),
+      //@ts-ignore
+      image: image?.name,
     };
+    //@ts-ignore
+    if (!!data.image) await Storage?.put(data.name, image);
     await API.graphql({
       query: createNoteMutation,
       variables: { input: data },
@@ -59,10 +74,12 @@ function MyApp({ Component, pageProps }: AppProps) {
     event.target.reset();
   }
 
-  async function deleteNote({ id } : {id: any}) {
+  async function deleteNote({ id, name }: { id: number; name: string }) {
     //@ts-ignore
-    const newNotes = notes.filter((note) => note?.id !== id);
+    const newNotes = notes.filter((note) => note.id !== id);
     setNotes(newNotes);
+    //@ts-ignore
+    await Storage?.remove(name);
     await API.graphql({
       query: deleteNoteMutation,
       variables: { input: { id } },
@@ -74,11 +91,39 @@ function MyApp({ Component, pageProps }: AppProps) {
       <GlobalStyle />
       <Layout>
         <ErrorBoundary>
+          {notes.map((note: any) => (
+            //@ts-ignore
+            <Flex
+              key={note?.id || note?.name}
+              direction="row"
+              justifyContent="center"
+              alignItems="center"
+            >
+              <Text as="strong" fontWeight={700}>
+                {/* //@ts-ignore */}
+                {note?.name}
+              </Text>
+              {/* //@ts-ignore */}
+              <Text as="span">{note.description}</Text>
+              {note.image && (
+                <>
+                  {/* <Image
+        src={note.image}
+        alt={`visual aid for ${notes.name}`}
+        style={{ width: 400 }}
+      /> */}
+                </>
+              )}
+              <Button variation="link" onClick={() => deleteNote(note)}>
+                Delete note
+              </Button>
+            </Flex>
+          ))}
           <Component {...pageProps} />
         </ErrorBoundary>
       </Layout>
     </ThemeProvider>
-  )
+  );
 }
 
-export default withAuthenticator(MyApp)
+export default withAuthenticator(MyApp);
