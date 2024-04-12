18045c48c821c211833e0781667fce16aa6f767b
bdoddi <bdoddi@my.bridgeport.edu>
2024-04-11 20:41:48 -0400

    Demo

--- pages/index.tsx
+++ pages/index.tsx
@@ -14,7 +14,7 @@ const Home: NextPage = () => {
 
       <main className={styles.main}>
         <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org"> BE YOU Application</a>
+          Welcome to <a href="https://nextjs.org"> Cloud computing</a>
         </h1>
 
         <p className={styles.description}>
